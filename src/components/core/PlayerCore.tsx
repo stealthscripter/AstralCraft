ba57c01b315de6a2a4bb581c5a0d9c1771e6a402
stealthscripter <natnael01ayele@gmail.com>
2025-02-21 17:44:21 +0300

    adding some layz

--- src/components/core/PlayerCore.tsx
+++ src/components/core/PlayerCore.tsx
@@ -1,10 +1,9 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { useState } from "react";
-import calculateFinger, { calculateWinner } from "../../utils/GameEngine";
+import { ChangeEvent, useState } from "react";
 
 function PlayerCore() {
-  const [finger , setFinger] = useState<number>(0)
+  const [selectedFingers, setSelectedFingers] = useState<string[]>([]);
   const userPick = useSelector(
     (state: RootState) => state.picksVariable.userPicks
   );
@@ -12,35 +11,232 @@ function PlayerCore() {
     (state: RootState) => state.picksVariable.computerPicks
   );
 
-  function handleStart(){
-    console.log("start the game")
-    const totalFinger = calculateFinger(finger)
-    console.log("total finger", totalFinger)
-    console.log(calculateWinner(totalFinger , userPick , computerPick))
+  function handleStart() {
+    console.log("start the game");
   }
+
+  function handleCheckbox(e: ChangeEvent<HTMLInputElement>) {
+    const { value, checked } = e.target;
+
+    setSelectedFingers((prev) =>
+      checked ? [...prev, value] : prev.filter((finger) => finger !== value)
+    );
+    
+  }
+
   return (
     <section className="grid grid-cols-12 gap-x-5 gap-y-5">
-      <section className="border border-amber-800 col-span-12 py-5 flex flex-col justify-center items-center">
-        <h1 className="text-xl">Player 1 </h1>
-        <h1>USer Picks</h1>
-        {userPick.map((pick) => (
-          <h1 key={pick} className="text-xl">
-            {pick}
-          </h1>
-        ))}
-        <h1>Computer Picks</h1>
-        {computerPick.map((pick) => (
-          <h1 key={pick} className="text-xl">
-            {pick}
-          </h1>
-        ))}
-        <label htmlFor="finger">Set Finger</label>
-        <input type="text" id="finger" className="border border-teal-500 mt-10" onChange={(e) => setFinger(Number(e.target.value))} />
+      <section className="border border-amber-800 py-5 col-span-12 flex justify-center">
+        <h1>How Many Fingers do you want to throw? </h1>
+      </section>
+      <section className="border border-amber-800 py-5 h-96 col-span-12 grid grid-cols-2 gap-x-18">
+        <section>
+          {/* Left Hand */}
+          <div className="w-full h-full grid grid-cols-5 grid-rows-5 gap-x-2">
+            {/* Finger 1 */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-28 w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("l-finger-1") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="l-finger-1"
+                value="l-finger-1"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="l-finger-1"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
 
-        <button className="mt-10 border border-teal-500 px-4 py-2 cursor-pointer hover:bg-red-50" onClick={handleStart}>start the game</button>
+            {/* Finger 2 */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-10 w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("l-finger-2") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="l-finger-2"
+                value="l-finger-2"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="l-finger-2"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Finger 3 */}
+            <div
+              className={`row-span-4 rounded-t-4xl w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("l-finger-3") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="l-finger-3"
+                value="l-finger-3"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="l-finger-3"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Finger 4 */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-10 w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("l-finger-4") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="l-finger-4"
+                value="l-finger-4"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="l-finger-4"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Thumb */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-40 -mb-16 rotate-[0.5rad] w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("l-finger-thumb") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="l-finger-thumb"
+                value="l-finger-thumb"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="l-finger-thumb"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Palm */}
+            <div className="bg-yellow-100 col-span-5 z-20 me-10"></div>
+          </div>
+        </section>
+        <section className="h-full">
+          {/* Right Hand */}
+          <div className="w-full h-full grid grid-cols-5 grid-rows-5 gap-x-2 scale-x-[-1]">
+            {/* Finger 1 */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-28 w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("r-finger-1") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="r-finger-1"
+                value="r-finger-1"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="r-finger-1"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Finger 2 */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-10 w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("r-finger-2") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="r-finger-2"
+                value="r-finger-2"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="r-finger-2"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Finger 3 */}
+            <div
+              className={`row-span-4 rounded-t-4xl w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("r-finger-3") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="r-finger-3"
+                value="r-finger-3"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="r-finger-3"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Finger 4 */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-10 w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("r-finger-4") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="r-finger-4"
+                value="r-finger-4"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="r-finger-4"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Thumb */}
+            <div
+              className={`row-span-4 rounded-t-4xl mt-40 -mb-16 rotate-[0.5rad] w-full relative transition-colors duration-300 ${
+                selectedFingers.includes("r-finger-thumb") ? "bg-red-500" : "bg-yellow-100"
+              }`}
+            >
+              <input
+                type="checkbox"
+                id="r-finger-thumb"
+                value="r-finger-thumb"
+                className="peer hidden"
+                onChange={handleCheckbox}
+              />
+              <label
+                htmlFor="r-finger-thumb"
+                className="h-full w-full block cursor-pointer"
+              ></label>
+            </div>
+
+            {/* Palm */}
+            <div className="bg-yellow-100 col-span-5 z-20 me-10"></div>
+          </div>
+        </section>
       </section>
 
-
+      <button className="border border-amber-400 p-4 col-span-3" onClick={() => {console.log(selectedFingers.length)}}>Start Again</button>
     </section>
   );
 }
